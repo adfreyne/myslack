e7be1e59a702fa027b6f2de1e91596a8a22c927c
Adriaan De Freyne <adriaandefreyne@gmail.com>
2018-06-08 16:20:07 +0200

    so passing the store now

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>Slack</div>
-    );
-  }
+    render () {
+        return (
+            <div>Slack</div>
+        );
+    }
 }
 
 export default App;
