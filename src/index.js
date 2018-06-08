6bd7490f747da16859dfe88c4f677af055c62d35
Adriaan De Freyne <adriaandefreyne@gmail.com>
2018-06-08 16:19:43 +0200

    more cleanup and setting up a redux store that is passed by a provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+/*global document*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
