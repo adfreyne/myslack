a6a3315f7aca73fd02d6f868480d1faed5daa58d
Adriaan De Freyne <adriaandefreyne@gmail.com>
2018-07-03 10:01:11 +0200

    clean up ChannelPage

--- src/pages/ChannelPage.js
+++ src/pages/ChannelPage.js
@@ -2,77 +2,71 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { send } from '../store/websocket';
 import PropTypes from 'prop-types';
-import { Field, reduxForm } from 'redux-form';
-//import { sendMessage } from '../store/initChannel.js';
 
 class ChannelPage extends Component {
     constructor () {
         super();
-        this.state = { channel: '', text: '' };
+        this.state = { channel: '', messageToChannel: '' };
     }
     render () {
         const {
-            //messageToChannel,
-            channel, text
+            channel, messageToChannel
         } = this.state;
 
         const {
-            connected,
-            received,
-            handleSubmit,
-            activeChannel
+            dispatch,
+            disconnected,
+            received
         } = this.props;
         let m = received.map((mess, index) => <li key={index}>{mess}</li>);
+
         return (
-            <div id="channelpage">
-                <form className="pure-form" onSubmit={handleSubmit}>
-                    Send message to {activeChannel}:
-                    {<div>
-                        <Field placeholder="channel" name="channel" component="input" type="text" value={channel} />
-                    </div>}
-                    <div>
-                        <Field placeholder="message" name="messageToChannel" component="input" type="text"
-                            value={text} />
-                    </div>
-                    <div>
-                        <button className="pure-button" type="submit"
-                            disabled={!connected}>
-                            send
-                        </button >
-                    </div>
-                    <hr />
-                    <div id="messagesOnChannelPage">Messages:
-                        <ul>{m}</ul>
-                    </div>
-                    <hr />
-                </form>
+            <div className="pure-form">
+                <hr />
+                <div>
+                    Send message to channel:
+                    <input
+                        value={channel}
+                        onChange={(e) => this.setState({ channel: e.target.value })}
+                    />
+                    <input
+                        value={messageToChannel}
+                        onChange={(e) => this.setState({ messageToChannel: e.target.value })} />
+                    <button className="pure-button" onClick={() => {
+                        let newMessage = "{\"command\": \"message\", \"channel\":\"" +
+                            channel + "\", \"message\":\"" + messageToChannel + "\"}";
+                        dispatch({ type: send, payload: newMessage });
+                    }}
+                    disabled={disconnected}>
+                        send
+                    </button>
+                </div>
+                <hr />
+                <div id="messagesOnChannelPage">Messages:
+                    <ul>{m}</ul>
+                </div>
             </div>
         );
     }
 }
-const onSubmit = ({ channel, text }, dispatch) => {
-    let newMessage = "{\"command\": \"message\", \"channel\":\"" +
-        channel + "\", \"message\":\"" + text + "\"}";
-    dispatch({ type: send, payload: newMessage });
-};
 
 const mapStateToProps = (state) => ({
     messages: state.messages.log,
-    connected: state.websocket.connected,
+    disconnected: !state.websocket.connected,
+    names: state.received.names,
     users: state.received.users,
-    channels: state.channels.channels,
-    received: state.received.received,
-    activeChannel: state.initChannel.activeChannel
+    channels: state.received.channels,
+    connected: state.websocket.connected,
+    received: state.received.received
+
 });
 ChannelPage.propTypes = {
-    connected: PropTypes.bool,
+    dispatch: PropTypes.func,
+    disconnected: PropTypes.bool,
+    messages: PropTypes.array,
     handleSubmit: PropTypes.func,
-    received: PropTypes.array,
-    activeChannel: PropTypes.string,
-    text: PropTypes.string
+    received: PropTypes.array
+
 };
-ChannelPage = reduxForm({
-    form: 'channelMessage',
-    onSubmit
-})(ChannelPage);
+
 export default connect(mapStateToProps)(ChannelPage);
